041d608f37e1aae7ed826102475db61ad854e18c
Pamela Tabak <pamelatabak@poli.ufrj.br>
2016-04-15 11:04:50 -0300

    minor changes

--- main.cpp
+++ main.cpp
@@ -27,39 +27,14 @@ int main(int argc, char * argv[])
 	 CImg<double> * gaussianPyramid = new CImg<double>[pyramidSize];
 
 	 CImg<double> reducedImage  = image;
-	 for (int p = 0; p < pyramidSize; p++)
-	 {
+	 for (int p = 0; p < pyramidSize; p++){
 	 	gaussianPyramid[p] = gPyramid.reduce(reducedImage);
 	 	reducedImage = gaussianPyramid[p];
 	 }
 
-	 // CImg<double> reducedImage1 = gPyramid.reduce(reducedImage);
-	 // CImg<double> reducedImage2 = gPyramid.reduce(reducedImage1);
-		 
-
-	 // for (int p = 0; p < pyramidSize; p++) {
-		// reducedImage = gPyramid.reduce(reducedImage);
-		// gaussianPyramid[p] = reducedImage;
-	 // }
-
 	 gaussianPyramid[1].display();
 
-	 delete gaussianPyramid;
-	 // delete gPyramid;
-
-	//  const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
-	//  image.blur(2.5);
- //     CImgDisplay main_disp(image,"Click a point"), draw_disp(visu,"Intensity profile");
- //     while (!main_disp.is_closed() && !draw_disp.is_closed()) {
-	//     main_disp.wait();
-	//     if (main_disp.button() && main_disp.mouse_y()>=0) {
-	//       const int y = main_disp.mouse_y();
-	//       visu.fill(0).draw_graph(image.get_crop(0,y,0,0,image.width()-1,y,0,0),red,1,1,0,255,0);
-	//       visu.draw_graph(image.get_crop(0,y,0,1,image.width()-1,y,0,1),green,1,1,0,255,0);
-	//       visu.draw_graph(image.get_crop(0,y,0,2,image.width()-1,y,0,2),blue,1,1,0,255,0).display(draw_disp);
-	//       }
-	// }
-
+	 // delete gaussianPyramid;
 
 	// if (argc == 2 && argv[1] == "--help") {
 	// 	std::cout << "Voce precisa passar duas imagens" << std::endl;
