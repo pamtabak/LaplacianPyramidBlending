b2b6c76da4203c3a99966a9c78e73e08ae94a7ca
Pamela Tabak <pamelatabak@poli.ufrj.br>
2016-04-21 00:37:09 -0300

    fixing errors

--- gaussianpyramid.hpp
+++ gaussianpyramid.hpp
@@ -14,48 +14,36 @@ public:
 	}
 
 	CImg<double> reduce (CImg<double> image) {
-		// Suavizar e reduzir Imagem pela metade de forma alternada linhas e colunas
-		int width       = image.width();
-		int height      = image.height();
-		int imageCanals = image.spectrum();
-
-		int modifiedWidth  = width;
-		int modifiedHeight = height;
-
-		if (width % 2 != 0){
-			modifiedWidth++;
-		}
-		if (height % 2 != 0){
-			modifiedHeight++;
-		}
-
-		CImg<double> reducedImage(modifiedWidth/2, modifiedHeight/2, 1, imageCanals,0);
-
-		for (int x = 0; x < modifiedWidth; x+=2) {
-			for (int y = 0; y < modifiedHeight; y+=2) {
-				// Iterating over each canal
-				for (int canal = 0; canal < imageCanals; canal++) {
-					// multiplying matrices
-					reducedImage(x/2,y/2,0,canal) = 0.0;
-					for (int i = -2; i <= 2; i++) {
-						for (int j = -2; j <= 2; j++) {
-							// Making exceptions for edges. We need to mirror the image
-							int newValueOfX = ((x + i) < 0) ? (-x - i - 1) : x + i;
-							newValueOfX = (newValueOfX >= width) ? ((2 * width) - x - i - 1) : newValueOfX;
-							int newValueOfY = ((y + j) < 0) ? (-y - j - 1) : y + j;
-							newValueOfY = (newValueOfY >= height) ? ((2 * height) - y - j - 1) : newValueOfY;
-
-							reducedImage(x/2,y/2,0,canal) += image(newValueOfX, newValueOfY,0,canal) * filter2d[i+2][j+2];
-						}	
-					}
-				}
-			}
-		}
-
-		return reducedImage;
+	    int width       = image.width();
+	    int height      = image.height();
+	    int imageCanals = image.spectrum();
+	 
+	    int newWidth = width/2;
+	    int newHeight = height/2;
+	 
+	    CImg<double> reducedImage(newWidth, newHeight, 1, imageCanals,0);
+	 
+	    for (int x = 0; x < newWidth; x++) {
+	        for (int y = 0; y < newHeight; y++) {
+	            for (int canal = 0; canal < imageCanals; canal++) {
+	                reducedImage(x,y,0,canal) = 0.0;
+	                for (int i = -2; i <= 2; i++) {
+	                    for (int j = -2; j <= 2; j++) {
+	                        int newValueOfX = ((x*2 + i) < 0) ? (-x*2 - i - 1) : x*2 + i;
+	                        newValueOfX = (newValueOfX >= width) ? ((2 * width) - x*2 - i - 1) : newValueOfX;
+	                        int newValueOfY = ((y*2 + j) < 0) ? (-y*2 - j - 1) : y*2 + j;
+	                        newValueOfY = (newValueOfY >= height) ? ((2 * height) - y*2 - j - 1) : newValueOfY;
+	                        
+	                        
+	                        reducedImage(x,y,0,canal) += image(newValueOfX, newValueOfY,0,canal) * filter2d[i+2][j+2];
+	                    }  
+	                }
+	            }
+	        }
+	    }
+	 
+	    return reducedImage;
 	}
-
-// 540 270 135 67
 
 	void generateFilter (double filter[5]) {
 		for (int i = 0; i < 5; i++) {
--- main.cpp
+++ main.cpp
@@ -34,112 +34,85 @@ int main(int argc, char * argv[]) {
 	}
 	else { maskString = "images/default.png";}
 
-	const int pyramidSize = 6;
-	
-	CImg<double> ** pyramidsG = new CImg<double>*[images.size()];
- 	CImg<double> ** pyramidsP = new CImg<double>*[images.size()];
-	for (int i = 0; i < images.size(); i++) {
-		pyramidsG[i] = new CImg<double>[pyramidSize];
- 		pyramidsP[i] = new CImg<double>[pyramidSize];
- 	}
+	const int pyramidSize = 5;
 
-	// Creating Images Laplacian Pyramids
-	for (int i = 0; i < images.size(); i++) {
-		const char *const imagePath = images[i].c_str();
-		
-		CImg<double> image(imagePath);
+ 	std::vector<std::vector<CImg <double> > > pyramidsP;
+ 	std::vector<std::vector<CImg <double> > > pyramidsG;
 
-		// Gaussian pyramid
-		GaussianPyramid gPyramid;
-
-		double filter[5] = {1.0/16, 4.0/16, 6.0/16, 4.0/16, 1.0/16};
-		gPyramid.generateFilter(filter);
-
-		pyramidsG[i][0] = image;
-		CImg<double> reducedImage  = image;
-		for (int p = 1; p < pyramidSize; p++){
-			pyramidsG[i][p] = gPyramid.reduce(reducedImage);
-			reducedImage = pyramidsG[i][p];
-		}
-
-		// Laplacian Pyramid
-		LaplacianPyramid lPyramid;
-
-		double laplacianFilter[5] = {0.5/8, 2.0/8, 3.0/8, 2.0/8, 0.5/8};
-		lPyramid.generateFilter(laplacianFilter);
-
-		for (int p = 1; p < pyramidSize; p++) {
-			pyramidsP[i][p - 1] = pyramidsG[i][p - 1] - lPyramid.expand(pyramidsG[i][p]);
-		}
-
-		/* The laplacian pyramid has one level less than the gaussian pyramid
-		so we repeat the last level of the gaussian pyramid */
-		pyramidsP[i][pyramidSize - 1] = pyramidsG[i][pyramidSize - 1];
-
-		// // Collapse Image
-		// CImg<double> collapseImage = pyramidsP[i][pyramidSize - 2] + lPyramid.expand(pyramidsP[i][pyramidSize - 1]);
-		// for (int a = pyramidSize - 3; a >= 0; a--){
-		// 	collapseImage = pyramidsP[i][a] + lPyramid.expand(collapseImage);
-		// }
-		// collapseImage.display();
-	}
-
-	// Creating Mask Gaussian Pyramid
-	const char *const maskPath = maskString.c_str();
-	CImg<double> image(maskPath);
-	std::vector<CImg<double> > maskGaussianPyramid;
-	GaussianPyramid maskGPyramid;
-
+ 	GaussianPyramid gPyramid;
 	double filter[5] = {1.0/16, 4.0/16, 6.0/16, 4.0/16, 1.0/16};
-	maskGPyramid.generateFilter(filter);
-
-	maskGaussianPyramid.push_back(image);
-	CImg<double> reducedImage  = image;
-	for (int p = 1; p < pyramidSize; p++){
-		CImg<double> maskReduced = maskGPyramid.reduce(reducedImage);
-		maskGaussianPyramid.push_back(maskReduced);
-		reducedImage = maskReduced;
-	}
-	// maskGaussianPyramid[pyramidSize - 1].display();
-
-
-	// Blending images
-	std::vector< CImg<double> > newLaplacianPyramid;
-	for (int i = 0; i < pyramidSize; i++) {
-		CImg<double> lsImageLaplacian(pyramidsP[0][i].width(), pyramidsP[0][i].height(),1,pyramidsP[0][i].spectrum(),0);
-		for (int canal = 0; canal < pyramidsP[0][i].spectrum(); canal++){
-			for (int x = 0; x < pyramidsP[0][i].width(); x++) {
-				for (int y = 0; y < pyramidsP[0][i].height(); y++) {
-					lsImageLaplacian(x,y,0,canal) = ((maskGaussianPyramid[i](x,y)/255) * (pyramidsP[0][i](x,y,0,canal))) 
-					+ ((1 - (maskGaussianPyramid[i](x,y)/255))*(pyramidsP[1][i](x,y,0,canal)));
-				}
-			}
-		}
-		newLaplacianPyramid.push_back(lsImageLaplacian);
-	}
+	gPyramid.generateFilter(filter);
 
 	LaplacianPyramid lPyramid;
+	lPyramid.generateFilter(filter);	
 
-	double laplacianFilter[5] = {0.5/8, 2.0/8, 3.0/8, 2.0/8, 0.5/8};
-	// double laplacianFilter[5] = {1.0/16, 4.0/16, 6.0/16, 4.0/16, 1.0/16};
-	lPyramid.generateFilter(laplacianFilter);
-	
-	CImg<double> collapsedImage = newLaplacianPyramid[pyramidSize - 2] + lPyramid.expand(newLaplacianPyramid[pyramidSize - 1]);
-	for (int i = pyramidSize - 3; i >= 0; i--){
-		collapsedImage = newLaplacianPyramid[i] + lPyramid.expand(collapsedImage);
-	}
-
-	collapsedImage.display();
-	// collapsedImage.normalize(0, 255);
-	// collapsedImage.save("result.png");
-	
 	for (int i = 0; i < images.size(); i++) {
-		delete [] pyramidsG[i];
-		delete [] pyramidsP[i];
+		const char *const imagePath = images[i].c_str();		
+		CImg<double> image(imagePath);
+
+		std::vector<CImg<double> > gaussianPyramids;
+		std::vector<CImg<double> > laplacianPyramids;
+		
+		gaussianPyramids.push_back(image);
+		CImg<double> reducedImage  = image;
+		for (int p = 1; p < pyramidSize; p++){
+			CImg<double> imageGenerated = gPyramid.reduce(reducedImage);
+			gaussianPyramids.push_back(imageGenerated);
+			reducedImage = imageGenerated;
+		}
+		
+		for (int p = 1; p < pyramidSize; p++) { 
+			laplacianPyramids.push_back(gaussianPyramids[p - 1] - 4*lPyramid.expand(gaussianPyramids[p]));
+		}
+		laplacianPyramids.push_back(gaussianPyramids[pyramidSize - 1]);
+
+		pyramidsG.push_back(gaussianPyramids);
+		pyramidsP.push_back(laplacianPyramids);
+
+		// Collapse Image
+		CImg<double> collapseImage = laplacianPyramids[pyramidSize - 2] + 4*lPyramid.expand(laplacianPyramids[pyramidSize - 1]);
+		for (int a = pyramidSize - 3; a >= 0; a--){
+			collapseImage = laplacianPyramids[a] + 4*lPyramid.expand(collapseImage);
+		}
+		collapseImage.display();
 	}
 
-	delete [] pyramidsG;
-	delete [] pyramidsP;
+	// // Creating Mask Gaussian Pyramid
+	// const char *const maskPath = maskString.c_str();
+	// CImg<double> image(maskPath);
+	// std::vector<CImg<double> > maskGaussianPyramid;
 
+	// maskGaussianPyramid.push_back(image);
+	// CImg<double> reducedImage  = image;
+	// for (int p = 1; p < pyramidSize; p++){
+	// 	CImg<double> maskReduced = gPyramid.reduce(reducedImage);
+	// 	maskGaussianPyramid.push_back(maskReduced);
+	// 	reducedImage = maskReduced;
+	// }
+
+	// // Blending images
+	// std::vector< CImg<double> > newLaplacianPyramid;
+	// for (int i = 0; i < pyramidSize; i++) {
+	// 	CImg<double> lsImageLaplacian(pyramidsP[0][i].width(), pyramidsP[0][i].height(),1,pyramidsP[0][i].spectrum(),0);
+	// 	for (int canal = 0; canal < pyramidsP[0][i].spectrum(); canal++){
+	// 		for (int x = 0; x < pyramidsP[0][i].width(); x++) {
+	// 			for (int y = 0; y < pyramidsP[0][i].height(); y++) {
+	// 				lsImageLaplacian(x,y,0,canal) = ((maskGaussianPyramid[i](x,y)/255) * (pyramidsP[0][i](x,y,0,canal))) 
+	// 				+ ((1.0 - (maskGaussianPyramid[i](x,y)/255))*(pyramidsP[1][i](x,y,0,canal)));
+	// 			}
+	// 		}
+	// 	}
+	// 	newLaplacianPyramid.push_back(lsImageLaplacian);
+	// }	
+
+	// newLaplacianPyramid[pyramidSize - 2].display();
+	// (4*lPyramid.expand(newLaplacianPyramid[pyramidSize - 1])).display();
+	// CImg<double> collapsedImage = newLaplacianPyramid[pyramidSize - 2] + 4*lPyramid.expand(newLaplacianPyramid[pyramidSize - 1]);
+	// // for (int i = pyramidSize - 3; i >= 0; i--){
+	// // 	collapsedImage = newLaplacianPyramid[i] + 4*lPyramid.expand(collapsedImage);
+	// // }
+	// collapsedImage.display();
+	
+	// collapsedImage.save("result.png");
 	return 0;
 }
